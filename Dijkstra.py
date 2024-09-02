f8cf1e56583b9ef47b113cc5d22fcb0e3ae27dbf
Janiopi <[janio072003@hotmail.com]>
2024-09-02 13:47:00 -0500

    Modularizacion del codigo

--- Dijkstra.py
+++ Dijkstra.py
@@ -40,29 +40,4 @@ class Graph:
 
         return distances
 
-g = Graph(7)
 
-g.add_vertex_data(0, 'A')
-g.add_vertex_data(1, 'B')
-g.add_vertex_data(2, 'C')
-g.add_vertex_data(3, 'D')
-g.add_vertex_data(4, 'E')
-g.add_vertex_data(5, 'F')
-g.add_vertex_data(6, 'G')
-
-g.add_edge(3, 0, 4)  # D - A, weight 5
-g.add_edge(3, 4, 2)  # D - E, weight 2
-g.add_edge(0, 2, 3)  # A - C, weight 3
-g.add_edge(0, 4, 4)  # A - E, weight 4
-g.add_edge(4, 2, 4)  # E - C, weight 4
-g.add_edge(4, 6, 5)  # E - G, weight 5
-g.add_edge(2, 5, 5)  # C - F, weight 5
-g.add_edge(2, 1, 2)  # C - B, weight 2
-g.add_edge(1, 5, 2)  # B - F, weight 2
-g.add_edge(6, 5, 5)  # G - F, weight 5
-
-# Dijkstra's algorithm from D to all vertices
-print("\nDijkstra's Algorithm starting from vertex D:")
-distances = g.dijkstra('D')
-for i, d in enumerate(distances):
-    print(f"Distance from D to {g.vertex_data[i]}: {d}")
